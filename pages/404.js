3e43ccb2df95d5fef305aeb4a98d92c70e89b8e9
silvioascruz <silvioascruz.ti@gmail.com>
2023-05-12 12:50:01 -0300

    Primeiro módulo do curso

---
+++ pages/404.js
@@ -0,0 +1,12 @@
+import Link from './src/components/Link';
+
+export default function Page404() {
+    return (
+        <div>
+            <h1>Página não encontrada - HTTP 404</h1>
+            <Link href="/">
+                Ir para home
+            </Link>
+        </div>
+    )
+}
---
+++ pages/faq.js
@@ -0,0 +1,12 @@
+import Link from './src/components/Link';
+
+export default function FAQPage() {
+    return (
+        <div>
+            <h1>Alura Cases - Página de perguntas FAQ</h1>
+            <Link href="/">
+                Ir para home
+            </Link>
+        </div>
+    )
+}
