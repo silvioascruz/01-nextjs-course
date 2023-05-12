1d478cc8c98156c5da58bb07dff0489656bd5aea
silvioascruz <silvioascruz.ti@gmail.com>
2023-05-12 12:53:53 -0300

    Corrige componente link estilizado

--- pages/src/components/LinkEstilizado/index.js
+++ pages/src/components/LinkEstilizado/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-const LinkEstilizado = React.forwardRef(({ onClick, href, children }, ref) => ){
+const LinkEstilizado = React.forwardRef(({ onClick, href, children }, ref) => {
     return (
         <a href={href} onClick={onClick} ref={ref} style={{ color: 'red' }}>
             {children}
